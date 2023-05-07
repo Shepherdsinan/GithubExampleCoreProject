19c707ac97e18cd5b808866f7823230d3558bce2
Shepherdsinan <65864888+Shepherdsinan@users.noreply.github.com>
2023-05-07 11:26:44 +0300

    Destination fixed and details added

---
+++ GithubExampleCoreProject/Controllers/DestinationController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Entityframework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubExampleCoreProject.Controllers;
+
+public class DestinationController : Controller
+{
+    private DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+    // GET
+    public IActionResult Index()
+    {
+        var values = destinationManager.TGetList();
+        return View(values);
+    }
+
+    [HttpGet]
+    public IActionResult DestinationDetails(int id)
+    {
+        var values = destinationManager.TGetByID(id);
+        return View(values);
+    }
+
+    [HttpPost]
+    public IActionResult DestinationDetails(Destination p)
+    {
+        return View();
+    }
+}
