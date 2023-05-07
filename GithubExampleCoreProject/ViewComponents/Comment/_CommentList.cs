27a1325ba9451978a2549f6085a5bf60de25e08e
Shepherdsinan <65864888+Shepherdsinan@users.noreply.github.com>
2023-05-07 11:24:35 +0300

    Yorum alt yapısı oluşturuldu

---
+++ GithubExampleCoreProject/ViewComponents/Comment/_CommentList.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GithubExampleCoreProject.ViewComponents.Comment;
+
+public class _CommentList: ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
