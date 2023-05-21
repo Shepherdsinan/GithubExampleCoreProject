0f533d7568cbb565dc563374bfae1e464d7cd692
Shepherdsinan <65864888+Shepherdsinan@users.noreply.github.com>
2023-05-22 00:00:27 +0300

    Login Controller // Signin and Signup view added

---
+++ GithubExampleCoreProject/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubExampleCoreProject.Controllers;
+[AllowAnonymous]
+public class LoginController : Controller
+{
+    [HttpGet]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+    // [HttpPost]
+    // public IActionResult SignUp()
+    // {
+    //     return View();
+    // }
+    [HttpGet]
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+}
