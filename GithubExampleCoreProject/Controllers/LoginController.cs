31a3330ab70053d0816b7940441d0aaeac6b0499
Sinan Çoban <sinan.coban@outlook.com>
2023-05-23 17:04:56 +0300

    Profile PreView page added

--- GithubExampleCoreProject/Controllers/LoginController.cs
+++ GithubExampleCoreProject/Controllers/LoginController.cs
@@ -6,23 +6,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace GithubExampleCoreProject.Controllers;
+
 [AllowAnonymous]
 public class LoginController : Controller
 {
     private readonly UserManager<AppUser> _userManager;
     private readonly SignInManager<AppUser> _signInManager;
 
-    public LoginController(UserManager<AppUser> userManager , SignInManager<AppUser> signInManager)
+    public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
     {
         _userManager = userManager;
         _signInManager = signInManager;
     }
-    
+
     [HttpGet]
     public IActionResult SignUp()
     {
         return View();
     }
+
     [HttpPost]
     public async Task<IActionResult> SignUp(UserRegisterViewModel p)
     {
@@ -46,17 +48,20 @@ public class LoginController : Controller
             {
                 foreach (var item in result.Errors)
                 {
-                    ModelState.AddModelError("",item.Description);
+                    ModelState.AddModelError("", item.Description);
                 }
             }
         }
+
         return View(p);
     }
+
     [HttpGet]
     public IActionResult SignIn()
     {
         return View();
     }
+
     [HttpPost]
     public async Task<IActionResult> SignIn(UserSignInViewModel p)
     {
@@ -65,13 +70,14 @@ public class LoginController : Controller
             var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
             if (result.Succeeded)
             {
-                return RedirectToAction("Index", "Destination");
+                return RedirectToAction("Index", "Profile", new { area = "Member" });
             }
             else
             {
                 return RedirectToAction("SignIn", "Login");
             }
         }
+
         return View();
     }
 }
