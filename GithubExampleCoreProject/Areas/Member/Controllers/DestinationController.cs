d071922cdb5ff0d65274e6d4e6f2355dcba6cf6c
Sinan Çoban <sinan.coban@outlook.com>
2023-05-23 16:07:44 +0300

    Example List to destination

---
+++ GithubExampleCoreProject/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Entityframework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubExampleCoreProject.Areas.Member.Controllers;
+
+[AllowAnonymous]
+[Area("Member")]
+public class DestinationController : Controller
+{
+    private DestinationManager _destinationManager = new DestinationManager(new EfDestinationDal());
+    public IActionResult Index()
+    {
+        var values = _destinationManager.TGetList();
+        return View(values);
+    }
+}
