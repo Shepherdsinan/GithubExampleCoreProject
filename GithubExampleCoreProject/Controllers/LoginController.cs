d86ea124c9ac8b2233bc4c972f59acf549c7e511
Sinan Çoban <sinan.coban@outlook.com>
2023-05-23 16:08:10 +0300

    Login codes writed

--- GithubExampleCoreProject/Controllers/LoginController.cs
+++ GithubExampleCoreProject/Controllers/LoginController.cs
@@ -10,10 +10,12 @@ namespace GithubExampleCoreProject.Controllers;
 public class LoginController : Controller
 {
     private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
 
-    public LoginController(UserManager<AppUser> userManager)
+    public LoginController(UserManager<AppUser> userManager , SignInManager<AppUser> signInManager)
     {
         _userManager = userManager;
+        _signInManager = signInManager;
     }
     
     [HttpGet]
@@ -29,7 +31,9 @@ public class LoginController : Controller
             Name = p.Name,
             Surname = p.Surname,
             Email = p.Mail,
-            UserName = p.Username
+            UserName = p.Username,
+            ImageUrl = "test",
+            Gender = "test"
         };
         if (p.Password == p.ConfirmPassword)
         {
@@ -51,6 +55,23 @@ public class LoginController : Controller
     [HttpGet]
     public IActionResult SignIn()
     {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> SignIn(UserSignInViewModel p)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Destination");
+            }
+            else
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+        }
         return View();
     }
 }
