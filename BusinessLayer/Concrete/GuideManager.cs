32c96c191bfb76aaadbf8590929d9ce19586671f
Sinan Çoban <sinan.coban@outlook.com>
2023-05-25 11:39:08 +0300

    Guide added

---
+++ BusinessLayer/Concrete/GuideManager.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete;
+
+public class GuideManager : IGuideService
+{
+    IGuideDal _guideDal;
+
+    public GuideManager(IGuideDal guideDal)
+    {
+        _guideDal = guideDal;
+    }
+
+    public void TAdd(Guide entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TDelete(Guide entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TUpdate(Guide entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Guide> TGetList()
+    {
+        return _guideDal.GetList();
+    }
+
+    public Guide TGetByID(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ GithubExampleCoreProject/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Entityframework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubExampleCoreProject.ViewComponents.MemberDashboard;
+
+public class _GuideList : ViewComponent
+{
+    private GuideManager _guideManager = new GuideManager(new EfGuideDal());
+
+    public IViewComponentResult Invoke()
+    {
+        var values = _guideManager.TGetList();
+        return View(values);
+    }
+}
