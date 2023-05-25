57cffe7f5bdb1dfbd59f00b46df3627eb466c0ee
Sinan Çoban <sinan.coban@outlook.com>
2023-05-25 11:39:32 +0300

    Platform setting added

---
+++ GithubExampleCoreProject/ViewComponents/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GithubExampleCoreProject.ViewComponents.MemberDashboard;
+
+public class _PlatformSetting : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }    
+}
