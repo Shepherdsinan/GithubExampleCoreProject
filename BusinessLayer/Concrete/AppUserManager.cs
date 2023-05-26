5f2edd7cb8266fcf0c24d4ca08f627cbc47ccec2
Sinan Çoban <sinan.coban@outlook.com>
2023-05-26 14:17:08 +0300

    Appuser structure complated

---
+++ BusinessLayer/Concrete/AppUserManager.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete;
+
+public class AppUserManager : IAppUserService
+{
+    private IAppUserDal _appUserDal;
+
+    public AppUserManager(IAppUserDal appUserDal)
+    {
+        _appUserDal = appUserDal;
+    }
+
+    public void TAdd(AppUser entity)
+    {
+        _appUserDal.Insert(entity);
+    }
+
+    public void TDelete(AppUser entity)
+    {
+        _appUserDal.Delete(entity);
+    }
+
+    public void TUpdate(AppUser entity)
+    {
+        _appUserDal.Update(entity);
+    }
+
+    public List<AppUser> TGetList()
+    {
+        return _appUserDal.GetList();
+    }
+
+    public AppUser TGetByID(int id)
+    {
+       return _appUserDal.GetByID(id);
+    }
+}
