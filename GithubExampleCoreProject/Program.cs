835089db9c5f82925287c6ae8fc8e7afa1cf34a6
Sinan Çoban <sinan.coban@outlook.com>
2023-05-26 08:23:22 +0300

    Admin member roote added

--- GithubExampleCoreProject/Program.cs
+++ GithubExampleCoreProject/Program.cs
@@ -47,4 +47,12 @@ app.UseEndpoints(endpoints =>
     );
 });
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name : "areas",
+        pattern : "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.Run();
