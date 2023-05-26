a8745621bddd51744dbda2effb38f88b9d536b8f
Sinan Çoban <sinan.coban@outlook.com>
2023-05-26 14:19:20 +0300

    Dependencies added

---
+++ BusinessLayer/Container/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Entityframework;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLayer.Container;
+
+public static class Extensions
+{
+    public static void ContainerDependencies(this IServiceCollection services)
+    {
+        services.AddScoped<ICommentService, CommentManager>();
+        services.AddScoped<ICommentDal, EfCommentDal>();
+        services.AddScoped<IDestinationService, DestinationManager>();
+        services.AddScoped<IDestinationDal, EfDestinationDal>();
+        services.AddScoped<IAppUserService, AppUserManager>();
+        services.AddScoped<IAppUserDal, EfAppUserDal>();
+    }
+}
