9e68082ab5274b2e4f6a5ff1f1786504a8b913c8
Sinan Çoban <sinan.coban@outlook.com>
2023-05-31 17:52:06 +0300

    Error page system added complate

---
+++ GithubExampleCoreProject/Controllers/ErrorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GithubExampleCoreProject.Controllers;
+
+public class ErrorPageController : Controller
+{
+    // GET
+    public IActionResult Error404(int code)
+    {
+        return View();
+    }
+}
