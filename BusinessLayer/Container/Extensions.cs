570e2e910ee843fb2cba7c0cf6a7237537def5b2
Sinan Çoban <sinan.coban@outlook.com>
2023-05-31 17:53:18 +0300

    Guide add page and guide list page added

--- BusinessLayer/Container/Extensions.cs
+++ BusinessLayer/Container/Extensions.cs
@@ -18,5 +18,7 @@ public static class Extensions
         services.AddScoped<IAppUserDal, EfAppUserDal>();
         services.AddScoped<IReservationService, ReservationManager>();
         services.AddScoped<IReservationDal, EfReservationDal>();
+        services.AddScoped<IGuideService,GuideManager>();
+        services.AddScoped<IGuideDal, EfGuideDal>();
     }
 }
