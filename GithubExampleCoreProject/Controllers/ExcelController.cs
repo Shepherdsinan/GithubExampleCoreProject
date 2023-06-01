9901937d3b704f10779b518fb4c61bdde49e8e9a
Sinan Çoban <sinan.coban@outlook.com>
2023-06-01 10:58:28 +0300

    Excel report system added

---
+++ GithubExampleCoreProject/Controllers/ExcelController.cs
@@ -0,0 +1,83 @@
+﻿using ClosedXML.Excel;
+using DataAccessLayer.Concrete;
+using GithubExampleCoreProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+namespace GithubExampleCoreProject.Controllers;
+
+public class ExcelController : Controller
+{
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public List<DestinationModel> DestinationList()
+    {
+        List<DestinationModel> destinationModels = new List<DestinationModel>();
+        using (var c = new Context())
+        {
+            destinationModels = c.Destinations.Select(x => new DestinationModel()
+            {
+                City = x.City,
+                DayNight = x.DayNight,
+                Price = x.Price,
+                Capacity = x.Capacity
+            }).ToList();
+        }
+
+        return destinationModels;
+    }
+
+    public IActionResult StaticExcelReport()
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        ExcelPackage excelPackage = new ExcelPackage();
+        var workSheet = excelPackage.Workbook.Worksheets.Add("Sayfa1");
+        workSheet.Cells[1, 1].Value = "Rota";
+        workSheet.Cells[1, 2].Value = "Rehber";
+        workSheet.Cells[1, 3].Value = "Kontenjan";
+        
+        workSheet.Cells[2, 1].Value = "Gürcistan Batum Turu";
+        workSheet.Cells[2, 2].Value = "Kadir Yıldız";
+        workSheet.Cells[2, 3].Value = "50";
+        
+        workSheet.Cells[3, 1].Value = "Sırbıstan - Makedonya Turu";
+        workSheet.Cells[3, 2].Value = "Zeynep Öztürk";
+        workSheet.Cells[3, 3].Value = "35";
+
+        var bytes = excelPackage.GetAsByteArray();
+        return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "dosya1.xlsx");
+    }
+
+    public IActionResult DestinationExcelReport()
+    {
+        using (var workbook = new XLWorkbook())
+        {
+            var workSheet = workbook.Worksheets.Add("Tur Listesi");
+            workSheet.Cell(1, 1).Value = "Şehir";
+            workSheet.Cell(1, 2).Value = "Konaklama Süresi";
+            workSheet.Cell(1, 3).Value = "Fiyat";
+            workSheet.Cell(1, 4).Value = "Kapasite";
+
+            int count = 2;
+            foreach (var item in DestinationList())
+            {
+                workSheet.Cell(count, 1).Value = item.City;
+                workSheet.Cell(count, 2).Value = item.DayNight;
+                workSheet.Cell(count, 3).Value = item.Price;
+                workSheet.Cell(count, 4).Value = item.Capacity;
+                count++;
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                workbook.SaveAs(stream);
+                var content = stream.ToArray();
+                return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "YeniListe.xlsx");
+            }
+        }
+    }
+}
---
+++ GithubExampleCoreProject/Models/DestinationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GithubExampleCoreProject.Models;
+
+public class DestinationModel
+{
+    public string City { get; set; }
+    public string DayNight { get; set; }
+    public double Price { get; set; }
+    public int Capacity { get; set; }
+}
