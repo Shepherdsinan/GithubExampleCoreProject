f0f2b583e89e5ca17d32b110aeeb50db37574985
Sinan Çoban <sinan.coban@outlook.com>
2023-06-01 10:58:47 +0300

    Guide validator added

---
+++ BusinessLayer/ValidationRules/GuideValidator.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules;
+
+public class GuideValidator : AbstractValidator<Guide>
+{
+    public GuideValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Lütfen rehber adını giriniz");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Lütfen rehber açıklamasını giriniz");
+        RuleFor(x => x.Image).NotEmpty().WithMessage("Lütfen rehber görselini seçiniz");
+        RuleFor(x => x.Name).MaximumLength(30).WithMessage("En fazla 30 karakter girilebilir");
+        RuleFor(x => x.Name).MinimumLength(8).WithMessage("En az 8 karakter girilmelidir");
+    }
+}
--- GithubExampleCoreProject/Areas/Admin/Controllers/GuideController.cs
+++ GithubExampleCoreProject/Areas/Admin/Controllers/GuideController.cs
@@ -1,8 +1,12 @@
 ﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace GithubExampleCoreProject.Areas.Admin.Controllers;
+
 [Area("Admin")]
 public class GuideController : Controller
 {
@@ -28,8 +32,22 @@ public class GuideController : Controller
     [HttpPost]
     public IActionResult AddGuide(Guide guide)
     {
-        _guideService.TAdd(guide);
-        return RedirectToAction("Index");
+        GuideValidator validator = new GuideValidator();
+        ValidationResult result = validator.Validate(guide);
+        if (result.IsValid)
+        {
+            _guideService.TAdd(guide);
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+            }
+
+            return View();
+        }
     }
 
     [HttpGet]
