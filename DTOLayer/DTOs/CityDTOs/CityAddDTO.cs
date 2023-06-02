aaad977fce4b8bee4411cccf67c15a4adab73fc1
Sinan Çoban <sinan.coban@outlook.com>
2023-06-02 14:54:02 +0300

    Add DTOs

---
+++ DTOLayer/DTOs/CityDTOs/CityAddDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DTOLayer.DTOs.CityDTOs;
+
+public class CityAddDTO
+{
+    public int CityId { get; set; }
+    public string CityName { get; set; }
+    public string CityCountry { get; set; }
+}
