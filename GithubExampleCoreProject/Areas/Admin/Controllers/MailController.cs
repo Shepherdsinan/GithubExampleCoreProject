2cf5245d08d2d0304f83f68b9f260130e24a2613
Sinan Çoban <sinan.coban@outlook.com>
2023-06-02 09:53:30 +0300

    Mail added

---
+++ GithubExampleCoreProject/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,36 @@
+﻿
+using GithubExampleCoreProject.Models;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace GithubExampleCoreProject.Areas.Admin.Controllers;
+[Area("Admin")]
+public class MailController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(MailRequest mailRequest)
+    {
+        MimeMessage mimeMessage = new MimeMessage();
+        MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin","example@gmail.com");
+        mimeMessage.From.Add(mailboxAddressFrom);
+        MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+        mimeMessage.To.Add(mailboxAddressTo);
+        var bodybuilder = new BodyBuilder();
+        bodybuilder.TextBody = mailRequest.Body;
+        mimeMessage.Body = bodybuilder.ToMessageBody();
+        mimeMessage.Subject = mailRequest.Subject;
+        SmtpClient client = new SmtpClient();
+        client.Connect("smtp.gmail.com",587,false);
+        client.Authenticate("example@gmail.com","123456");
+        client.Send(mimeMessage);
+        client.Disconnect(true);
+        return View();
+    }
+}
---
+++ GithubExampleCoreProject/Models/MailRequest.cs
@@ -0,0 +1,12 @@
+﻿using MimeKit;
+
+namespace GithubExampleCoreProject.Models;
+
+public class MailRequest
+{
+    public string SenderMail { get; set; }
+    public string ReceiverMail { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public string Name { get; set; }
+}
