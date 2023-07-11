a3bd55745af87092ee6fc1e63e3751a1b24fc9bf
Sinan Çoban <sinan.coban@outlook.com>
2023-07-11 18:00:39 +0300

    Hotel search list api added

---
+++ GithubExampleCoreProject/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using GithubExampleCoreProject.Areas.Admin.Models;
+
+
+namespace GithubExampleCoreProject.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[AllowAnonymous]
+public class BookingHotelSearchController : Controller
+{
+
+    public async Task<IActionResult> Index()
+    {
+        
+        var client = new HttpClient();
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?order_by=popularity&adults_number=2&checkin_date=2023-09-27&filter_by_currency=AED&dest_id=-553173&locale=en-gb&checkout_date=2023-09-28&units=metric&room_number=1&dest_type=city&include_adjacency=true&children_number=2&page_number=0&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1"),
+            Headers =
+            {
+                { "X-RapidAPI-Key", "0ea86332bemsh96921789a8a3591p1de5a2jsn4af32eed2961" },
+                { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+            },
+        };
+        using (var response = await client.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            //var bodyReplace = body.Replace(".", "");
+            var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel>(body);
+            return View(values.results);
+        }
+
+        #region eski api
+
+        /*var client = new HttpClient();
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?room_number=1&dest_type=city&order_by=popularity&dest_id=-1456928&locale=en-gb&checkin_date=2023-01-27&filter_by_currency=EUR&checkout_date=2023-01-28&adults_number=2&units=metric&children_ages=5%2C0&include_adjacency=true&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_number=2"),
+            Headers =
+            {
+                { "X-RapidAPI-Key", "cb5ee15da1mshb46d59d679af3abp1fe84cjsn167590fdc0cc" },
+                { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+            },
+        };
+        using (var response = await client.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            var bodyReplace = body.Replace(".", "");
+            var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel>(bodyReplace);
+            return View(values.result);
+        }*/
+        #endregion
+    }
+}
---
+++ GithubExampleCoreProject/Areas/Admin/Models/BookingHotelSearchViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GithubExampleCoreProject.Areas.Admin.Models
+{
+    public class BookingHotelSearchViewModel
+    {
+        public int count { get; set; }
+        public MapPageFields mapPageFields { get; set; }
+        public Results[] results { get; set; }
+
+
+        public class MapPageFields
+        {
+            public BoundingBox boundingBox { get; set; }
+            public PropertyAnnotations[] propertyAnnotations { get; set; }
+        }
+
+        public class BoundingBox
+        {
+            public double swLat { get; set; }
+            public double swLon { get; set; }
+            public double neLon { get; set; }
+            public double neLat { get; set; }
+        }
+
+        public class PropertyAnnotations
+        {
+            public double latitude { get; set; }
+            public double longitude { get; set; }
+        }
+
+        public class Results
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public int mainPhotoId { get; set; }
+            public string photoMainUrl { get; set; }
+            public string[] photoUrls { get; set; }
+            public int position { get; set; }
+            public int rankingPosition { get; set; }
+            public string countryCode { get; set; }
+            public double latitude { get; set; }
+            public double longitude { get; set; }
+            public PriceBreakdown priceBreakdown { get; set; }
+            public string currency { get; set; }
+            public Checkin checkin { get; set; }
+            public Checkout checkout { get; set; }
+            public string checkoutDate { get; set; }
+            public string checkinDate { get; set; }
+            public double reviewScore { get; set; }
+            public string reviewScoreWord { get; set; }
+            public int reviewCount { get; set; }
+            public int qualityClass { get; set; }
+            public bool isFirstPage { get; set; }
+            public int accuratePropertyClass { get; set; }
+            public int propertyClass { get; set; }
+            public int ufi { get; set; }
+            public string wishlistName { get; set; }
+            public int optOutFromGalleryChanges { get; set; }
+        }
+
+        public class PriceBreakdown
+        {
+            public StrikethroughPrice strikethroughPrice { get; set; }
+            public GrossPrice grossPrice { get; set; }
+            public object[] taxExceptions { get; set; }
+            public BenefitBadges[] benefitBadges { get; set; }
+        }
+
+        public class StrikethroughPrice
+        {
+            public double value { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class GrossPrice
+        {
+            public string currency { get; set; }
+            public double value { get; set; }
+        }
+
+        public class BenefitBadges
+        {
+            public string identifier { get; set; }
+            public string text { get; set; }
+            public string explanation { get; set; }
+            public string variant { get; set; }
+        }
+
+        public class Checkin
+        {
+            public string fromTime { get; set; }
+            public string untilTime { get; set; }
+        }
+
+        public class Checkout
+        {
+            public string fromTime { get; set; }
+            public string untilTime { get; set; }
+        }
+    }
+}
